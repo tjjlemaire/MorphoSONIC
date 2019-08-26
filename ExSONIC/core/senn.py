corrected SENN fiber titration, and included strength-duration curve computation in test_SENN.py

ExSONIC/core/senn.py
@@ -3,7 +3,7 @@
 # @Email: theo.lemaire@epfl.ch
 # @Date:   2019-06-27 15:18:44
 # @Last Modified by:   Theo Lemaire
-# @Last Modified time: 2019-08-23 20:21:19
+# @Last Modified time: 2019-08-26 10:34:29
 
 import abc
 import pickle
@@ -13,7 +13,7 @@ from inspect import signature
 
 from PySONIC.core import Model
 from PySONIC.neurons import *
-from PySONIC.utils import si_format, pow10_format, logger, plural, binarySearch
+from PySONIC.utils import si_format, pow10_format, logger, plural, binarySearch, pow2Search
 from PySONIC.constants import *
 from PySONIC.postpro import detectSpikes
 
@@ -346,7 +346,10 @@ class SennFiber(metaclass=abc.ABCMeta):
             :param method: integration method
             :return: determined threshold amplitude (Pa)
         '''
-        Arange, A_conv_thr = self.getArange(psource)
+        Amin, Amax = self.getArange(psource)
+        A_conv_thr = np.abs(Amax - Amin) / 1e3
+        Athr = pow2Search(self.titrationFunc, [psource, tstim, toffset, PRF, DC], 1, Amin, Amax)
+        Arange = (Athr / 2, Athr)
         return binarySearch(
             self.titrationFunc, [psource, tstim, toffset, PRF, DC], 1, Arange, A_conv_thr)
 
@@ -402,7 +405,7 @@ class SennFiber(metaclass=abc.ABCMeta):
 class VextSennFiber(SennFiber):
 
     simkey = 'senn_ESTIM'
-    Ve_thr = 500.  # mV
+    Ve_range = (1e0, 1e3)  # mV
 
     def __init__(self, pneuron, fiberD, nnodes, **kwargs):
         mechname = pneuron.name + 'auto'
@@ -448,9 +451,7 @@ class VextSennFiber(SennFiber):
         return value
 
     def getArange(self, psource):
-        Arange = (0, psource.getMaxAmp(self, self.Ve_thr))
-        A_conv_thr = np.abs(Arange[1] - Arange[0]) / 1e3
-        return Arange, A_conv_thr
+        return [psource.reverseComputeAmp(self, x) for x in self.Ve_range]
 
     def titrationFunc(self, args):
         psource, A, *args = args

tests/test_SENN.py
@@ -3,7 +3,7 @@
 # @Email: theo.lemaire@epfl.ch
 # @Date:   2019-08-19 19:30:19
 # @Last Modified by:   Theo Lemaire
-# @Last Modified time: 2019-08-23 20:35:09
+# @Last Modified time: 2019-08-26 10:45:02
 
 import numpy as np
 import logging
@@ -17,9 +17,6 @@ from ExSONIC.plt import SectionCompTimeSeries
 
 
 class TestSenn(TestBase):
-    ''' Run IintraNode (ESTIM) and SonicNode (ASTIM) simulations with manually and automatically created
-        NMODL membrane mechanisms, and compare results.
-    '''
 
     def test_ESTIM(self, is_profiled=False):
 
@@ -36,7 +33,6 @@ class TestSenn(TestBase):
         mode = 'cathode'
 
         # Stimulation Parameters
-        tstim = 100e-6  # s
         toffset = 3e-3  # s
         PRF = 100.
         DC = 1.
@@ -44,18 +40,31 @@ class TestSenn(TestBase):
         # Create point-source electrode
         psource = CurrentPointSource(x0, z0, mode=mode)
 
-        # Titrate
+        # Titrate for a specific duration and simulate fiber at threshold current
+        tstim = 100e-6  # s
         Ithr = fiber.titrate(psource, tstim, toffset, PRF, DC)
-        logger.info(f'Ithr = {si_format(Ithr)}A')
-
-        # Simulate fiber at threshold current
+        logger.info(f'tstim = {si_format(tstim)}s -> Ithr = {si_format(Ithr)}A')
         data, meta = fiber.simulate(psource, Ithr, tstim, toffset, PRF, DC)
 
-        # Plot resulting membrane potential traces
-        sections = fiber.ids
-        varname = 'Vm'
-        filepath = [(data, meta)]
-        fig = SectionCompTimeSeries(filepath, varname, sections).render()
+        # Compute strength-duration curve
+        durations = np.logspace(-4, 0, 20)  # s
+        Ithrs = np.array([fiber.titrate(psource, x, toffset, PRF, DC) for x in durations])  # A
+
+        # Plot membrane potential traces for specific duration at threshold current
+        fig1 = SectionCompTimeSeries([(data, meta)], 'Vm', fiber.ids).render()
+
+        # Plot strength-duration curve
+        fs = 12
+        fig2, ax = plt.subplots()
+        ax.set_title(f'{fiber} - strength-duration curve', fontsize=fs)
+        ax.set_xlabel('duration (ms)', fontsize=fs)
+        ax.set_ylabel('threshold cathodic current (mA)', fontsize=fs)
+        ax.set_xscale('log')
+        ax.set_yscale('log')
+        ax.plot(durations * 1e3, -Ithrs * 1e3)
+        for item in ax.get_xticklabels() + ax.get_yticklabels():
+            item.set_fontsize(fs)
+
         plt.show()
 
 