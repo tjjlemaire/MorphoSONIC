corrected SennFiber model (interD/fiberD ratio) and validated VextSennFiber model numerically against Reilly 1985 (in test_SENN.py)

ExSONIC/core/senn.py
@@ -3,7 +3,7 @@
 # @Email: theo.lemaire@epfl.ch
 # @Date:   2019-06-27 15:18:44
 # @Last Modified by:   Theo Lemaire
-# @Last Modified time: 2019-08-26 13:44:45
+# @Last Modified time: 2019-08-29 11:36:05
 
 import abc
 import pickle
@@ -11,7 +11,7 @@ import numpy as np
 import pandas as pd
 from inspect import signature
 
-from PySONIC.core import Model
+from PySONIC.core import Model, PointNeuron
 from PySONIC.neurons import *
 from PySONIC.utils import si_format, pow10_format, logger, plural, binarySearch, pow2Search
 from PySONIC.constants import *
@@ -33,10 +33,22 @@ class SennFiber(metaclass=abc.ABCMeta):
         ''' Keyword used to characterize stimulation modality. '''
         raise NotImplementedError
 
-    def __init__(self, pneuron, fiberD, nnodes, rs=1e2):
+    def __init__(self, pneuron, fiberD, nnodes, rs=1e2, nodeL=2.5e-6, d_ratio=0.7):
+        ''' Initialize fiber model.
 
+            :param pneuron: point-neuron model object
+            :param fiberD: fiber outer diameter (m)
+            :param nnodes: number of nodes
+            :param rs: axoplasmic resistivity (Ohm.cm)
+            :param nodeL: nominal node length (m)
+            :param d_ratio: ratio of axon (inner-myelin) and fiber (outer-myelin) diameters
+        '''
+        if not isinstance(pneuron, PointNeuron):
+            raise TypeError(f'{pneuron} is not a valid PointNeuron instance')
         if nnodes % 2 == 0:
             raise ValueError('Number of nodes must be odd')
+        if fiberD <= 0:
+            raise ValueError('Fiber diameter must be positive')
 
         # Assign attributes
         self.pneuron = pneuron
@@ -45,10 +57,10 @@ class SennFiber(metaclass=abc.ABCMeta):
         self.fiberD = fiberD
 
         # Define fiber geometrical parameters
-        self.nodeD = 0.7 * fiberD  # m
+        self.nodeD = d_ratio * self.fiberD  # m
         self.nodeL = 2.5e-6  # m
-        self.interD = fiberD  # m
-        self.interL = 100 * fiberD  # m
+        self.interD = d_ratio * self.fiberD  # m
+        self.interL = 100 * self.fiberD  # m
 
         # Compute nodal and internodal axial resistance ()
         self.R_node = self.resistance(self.nodeD, self.nodeL)  # Ohm
@@ -64,7 +76,7 @@ class SennFiber(metaclass=abc.ABCMeta):
 
     def __repr__(self):
         ''' Explicit naming of the model instance. '''
-        return f'SennFiber({self.strBiophysics()}, {self.strNodes()}, d = {self.interD * 1e6:.1f} um)'
+        return f'SennFiber({self.strBiophysics()}, {self.strNodes()}, d = {self.fiberD * 1e6:.1f} um)'
 
     @staticmethod
     def inputs():
@@ -131,31 +143,32 @@ class SennFiber(metaclass=abc.ABCMeta):
         '''
         if rs is None:
             rs = self.rs
-        return 4 * rs * L / (np.pi * d**2) * 1e-2
+        return 4 * rs * L / (np.pi * d**2) * 1e-2  # Ohm
 
     def setResistivity(self):
         ''' Set sections axial resistivity, corrected to account for internodes and membrane capacitance
             in the Q-based differentiation scheme. '''
 
-        logger.debug(f'setting nodal resistivity to {self.rs:.0f} Ohm.cm')
-        for sec in self.sections.values():
-            sec.Ra = self.rs
+        logger.debug(f'nominal nodal resistivity: rs = {self.rs:.0f} Ohm.cm')
+        rho_nodes = np.ones(self.nnodes) * self.rs  # Ohm.cm
 
-        logger.debug('adjusting resistivities to account for internodal sections')
-        for i, sec in enumerate(self.sections.values()):
-            R_extra = 0
-            for ind in [0, self.nnodes - 1]:
-                if i != ind:
-                    R_extra += self.R_inter / 2
-            sec.Ra *= (1 + R_extra / self.R_node)
+        # Adding extra resistivity to account for half-internodal resistance
+        # for each connected side of each node
+        logger.debug('adding extra-resistivity to account for internodal resistance')
+        R_extra = np.array([self.R_inter / 2] + [self.R_inter] * (self.nnodes - 2) + [self.R_inter / 2])  # Ohm
+        rho_extra = R_extra * self.rs / self.R_node  # Ohm.cm
+        rho_nodes += rho_extra  # Ohm.cm
 
         # In case the axial coupling variable is v (an alias for membrane charge density),
         # multiply resistivity by membrane capacitance to ensure consistency of Q-based
         # differential scheme, where Iax = dV / r = dQ / (r * cm)
         if self.connector is None or self.connector.vref == 'v':
             logger.debug('adjusting resistivities to account for Q-based differential scheme')
-        for sec in self.sections.values():
-            sec.Ra *= self.pneuron.Cm0 * 1e2
+            rho_nodes *= self.pneuron.Cm0 * 1e2  # Ohm.cm
+
+        # Assigning resistivities to sections
+        for sec, rho in zip(self.sections.values(), rho_nodes):
+            sec.Ra = rho
 
     def setTopology(self):
         ''' Connect the sections in series. '''
@@ -460,8 +473,7 @@ class SennFiber(metaclass=abc.ABCMeta):
     @staticmethod
     def getSpikeAmp(data, key='Vm'):
         amps = np.array([np.ptp(df[key].values) for df in data.values()])
-        return np.mean(amps)
-
+        return amps.min(), amps.max()
 
 
 class VextSennFiber(SennFiber):
@@ -471,7 +483,7 @@ class VextSennFiber(SennFiber):
 
     def __init__(self, pneuron, fiberD, nnodes, **kwargs):
         mechname = pneuron.name + 'auto'
-        self.connector = SeriesConnector(vref=f'Vm_{mechname}', rmin=None)
+        # self.connector = SeriesConnector(vref=f'Vm_{mechname}', rmin=None)
         # self.connector = SeriesConnector(vref='v', rmin=None)
         self.connector = None
         super().__init__(pneuron, fiberD, nnodes, **kwargs)

tests/test_SENN.py
@@ -3,7 +3,7 @@
 # @Email: theo.lemaire@epfl.ch
 # @Date:   2019-08-19 19:30:19
 # @Last Modified by:   Theo Lemaire
-# @Last Modified time: 2019-08-26 13:47:42
+# @Last Modified time: 2019-08-29 11:32:45
 
 import numpy as np
 import logging
@@ -18,61 +18,115 @@ from ExSONIC.plt import SectionCompTimeSeries, strengthDurationCurve
 
 class TestSenn(TestBase):
 
-    def test_ESTIM(self, is_profiled=False):
+    def test_Reilly1985(self, is_profiled=False):
+        ''' Run SENN fiber simulation with identical parameters as in Reilly 1985, Fig2.,
+            and compare resulting threshold current, spike amplitude and conduction velocity.
 
-        # Fiber model
-        pneuron = getPointNeuron('FH')
-        fiberD = 20e-6  # m
+            Reference: Reilly, J.P., Freeman, V.T., and Larkin, W.D. (1985). Sensory effects
+            of transient electrical stimulation--evaluation with a neuroelectric model.
+            IEEE Trans Biomed Eng 32, 1001–1011.
+        '''
+
+        # Fiber model parameters
+        pneuron = getPointNeuron('FH')  # FrankenHaeuser-Huxley membrane equations
+        fiberD = 20e-6                  # fiber diameter (m)
         nnodes = 11
-        rs = 110.0  # Ohm.cm
-        fiber = VextSennFiber(pneuron, fiberD, nnodes, rs=rs)
+        rho_a = 110.0                   # axoplasm resistivity (Ohm.cm, from McNeal 1976)
+        d_ratio = 0.7                   # axon / fiber diameter ratio (from McNeal 1976)
+        nodeL = 2.5e-6                  # node length (m, from McNeal 1976)
+        fiber = VextSennFiber(pneuron, fiberD, nnodes, rs=rho_a, nodeL=nodeL, d_ratio=d_ratio)
 
-        # Electrode type and location
-        z0 = fiber.interL  # one internodal distance away from fiber m
-        x0 = 0.  # m
-        mode = 'cathode'
+        # Electrode and stimulation parameters (from Reilly 1985)
+        rho_e = 300.0      # resistivity of external medium (Ohm.cm, from McNeal 1976)
+        z0 = fiber.interL  # point-electrode to fiber distance (m, 1 internode length)
+        x0 = 0.            # point-electrode located above central node (m)
+        mode = 'cathode'   # cathodic pulse
+        tstim = 100e-6     # s
+        toffset = 3e-3     # s
+        PRF = 100.         # Hz
+        DC = 1.            # -
 
-        # Stimulation Parameters
-        toffset = 3e-3  # s
-        PRF = 100.
-        DC = 1.
+        # Reference outputs (from Reilly 1985)
+        Ithr_ref = -0.68e-3  # threshold cathodic excitation current amplitude (A)
+        cv_ref = 43.0        # conduction velocity (m/s)
+        dV_ref = (105, 115)  # spike amplitude (mV)
 
         # Create point-source electrode
-        psource = CurrentPointSource(x0, z0, mode=mode)
+        psource = CurrentPointSource(x0, z0, rho=rho_e, mode=mode)
 
         # Titrate for a specific duration and simulate fiber at threshold current
-        tstim = 100e-6  # s
-        Ithr = fiber.titrate(psource, tstim, toffset, PRF, DC)
+        logger.info(f'Running titration for {si_format(tstim)}s pulse')
+        Ithr = fiber.titrate(psource, tstim, toffset, PRF, DC)  # A
         data, meta = fiber.simulate(psource, Ithr, tstim, toffset, PRF, DC)
 
         # Compute conduction velocity and spike amplitude from resulting data
-        cv = fiber.getConductionVelocity(data)
-        dV = fiber.getSpikeAmp(data)
+        cv = fiber.getConductionVelocity(data)  # m/s
+        dVmin, dVmax = fiber.getSpikeAmp(data)  # mV
 
-        # Compute strength-duration curve
+        # Log output metrics
+        logger.info(f'threshold current: Ithr = {si_format(Ithr)}A (ref: {si_format(Ithr_ref)}A)')
+        logger.info(f'conduction velocity: v = {cv:.1f} m/s (ref: {cv_ref:.1f} m/s)')
+        logger.info(f'spike amplitude: dV = {dVmin:.1f}-{dVmax:.1f} mV (ref: {dV_ref[0]:.1f}-{dV_ref[1]:.1f} mV)')
+
+        # Plot membrane potential traces for specific duration at threshold current
+        fig = SectionCompTimeSeries([(data, meta)], 'Vm', fiber.ids).render()
+
+    def test_Sweeney1987(self, is_profiled=False):
+        ''' Run SENN fiber simulation with identical parameters as in Sweeney 1987, Figs 2 & 3.,
+            and compare resulting threshold current, spike amplitude and conduction velocity,
+            strength-duration curves, rheobase current and chronaxie.
+
+            Reference: Sweeney, J.D., Mortimer, J.T., and Durand, D. (1987). Modeling of
+            mammalian myelinated nerve for functional neuromuscular stimulation. IEEE 9th
+            Annual Conference of the Engineering in Medicine and Biology Society 3, 1577–1578.
+        '''
+        return
+
+        # Fiber model parameters
+        pneuron = getPointNeuron('sweeney')  # mammalian fiber membrane equations
+        fiberD = 10e-6                       # fiber diameter (m)
+        nnodes = 19
+        rho_a = 54.7                         # axoplasm resistivity (Ohm.cm)
+        d_ratio = 0.6                        # axon / fiber diameter ratio
+        nodeL = 1.5e-6                       # node length (m)
+        fiber = IinjSennFiber(pneuron, fiberD, nnodes, rs=rho_a, nodeL=nodeL, d_ratio=d_ratio)
+
+        # Intracellular stimulation parameters
+        tstim = 10e-6   # s
+        toffset = 3e-3  # s
+        PRF = 100.      # Hz
+        DC = 1.         # -
+
+        # Output metrics (from Sweeney 1987)
+        cv_ref = 57.0       # conduction velocity (m/s)
+        dV_ref = (95, 105)  # spike amplitude (mV)
+        tau_ref = 25.9e-6   # chronaxie from SD curve (s)
+
+        # Titrate for a specific duration and simulate fiber at threshold current
+        logger.info(f'Running titration for {si_format(tstim)}s pulse')
+        Ithr = fiber.titrate(psource, tstim, toffset, PRF, DC)  # A
+        data, meta = fiber.simulate(psource, Ithr, tstim, toffset, PRF, DC)
+
+        # Compute conduction velocity and spike amplitude from resulting data
+        cv = fiber.getConductionVelocity(data)  # m/s
+        dVmin, dVmax = fiber.getSpikeAmp(data)  # mV
+
+        # Log output metrics
+        logger.info(f'threshold current: Ithr = {si_format(Ithr)}A')
+        logger.info(f'conduction velocity: v = {cv:.1f} m/s (ref: {cv_ref:.1f} m/s)')
+        logger.info(f'spike amplitude: dV = {dVmin:.1f}-{dVmax:.1f} mV (ref: {dV_ref[0]:.1f}-{dV_ref[1]:.1f} mV)')
+
+        # Compute and plot strength-duration curve
         durations = np.logspace(-5, -1, 30)  # s
         Ithrs = np.array([fiber.titrate(psource, x, toffset, PRF, DC) for x in durations])  # A
         tau = psource.chronaxie(durations, Ithrs)  # s
-
-        # Log output metrics
-        logger.info(f'tstim = {si_format(tstim)}s -> Ithr = {si_format(Ithr)}A')
-        logger.info(f'conduction velocity: v = {cv:.1f} m/s, spike amplitude: dV = {dV:.1f} mV')
-        logger.info(f'strength-duration curve: chronaxie = {si_format(tau, 1)}s')
-
-
-        # Plot membrane potential traces for specific duration at threshold current
-        fig1 = SectionCompTimeSeries([(data, meta)], 'Vm', fiber.ids).render()
+        logger.info(f'strength-duration curve: chronaxie = {si_format(tau, 1)}s (ref = {si_format(tau_ref, 1)}s)')
 
         # Plot strength-duration curve
-        fig2 = strengthDurationCurve(fiber, psource, durations, Ithrs)
-
-        plt.show()
-
-    def test_ASTIM(self, is_profiled=False):
-        pass
-
+        fig = strengthDurationCurve(fiber, psource, durations, Ithrs)
 
 
 if __name__ == '__main__':
     tester = TestSenn()
     tester.main()
+    plt.show()