53dc1a67d9abea3c74138c88bfc83288b18db5ed
Theo Lemaire <theo.lemaire@epfl.ch>
2019-12-06 13:41:46 +0100

    added handling of mismatching indexes for zero crossings and peaks in SennFiber.getSpikesTimings

--- ExSONIC/core/psource.py
+++ ExSONIC/core/psource.py
@@ -3,7 +3,7 @@
 # @Email: theo.lemaire@epfl.ch
 # @Date:   2019-08-23 09:43:18
 # @Last Modified by:   Theo Lemaire
-# @Last Modified time: 2019-12-03 14:34:58
+# @Last Modified time: 2019-12-03 18:32:18
 
 import abc
 import numpy as np
