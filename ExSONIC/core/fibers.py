151288460d18d944b02a44c1ee785f92908e51e5
Elena Vicari <elena.vicari@epfl.ch>
2019-12-10 13:54:27 +0100

    correction in unmyelinatedFiberConvergence and creation of the convergence function with the fiber length

--- ExSONIC/core/fibers.py
+++ ExSONIC/core/fibers.py
@@ -53,7 +53,7 @@ def myelinatedFiber(fiber_class, pneuron, fiberD, nnodes, rs, nodeL, d_ratio, in
     return fiber_class(pneuron, nnodes, rs, nodeD, nodeL, interD, interL, **kwargs)
 
 
-def unmyelinatedFiber(fiber_class, pneuron, fiberD, rs, fiberL, maxNodeL=50e-6, **kwargs):
+def unmyelinatedFiber(fiber_class, pneuron, fiberD, rs, fiberL, maxNodeL=None, **kwargs):
     ''' Create a single-cable unmyelinated fiber model.
 
         :param fiber_class: class of fiber to be instanced
@@ -68,7 +68,10 @@ def unmyelinatedFiber(fiber_class, pneuron, fiberD, rs, fiberL, maxNodeL=50e-6, 
     if fiberD <= 0:
         raise ValueError('fiber diameter must be positive')
     if fiberL <= 0:
-        raise ValueError('fiber length must be positive')
+        raise ValueError('fiber length must be positive')   
+    if maxNodeL is None:
+        nodelength_lin = fiberD * 12.1 + 15.0e-6  #um
+        maxNodeL = min(nodelength_lin, 50e-6)
     if maxNodeL <= 0. or maxNodeL > fiberL:
         raise ValueError('maximum node length must be in [0, fiberL]')
 
@@ -140,7 +143,7 @@ def unmyelinatedFiberConvergence(pneuron, fiberD, rs, fiberL, maxNodeL_range, pp
 
         # If max node length not already in the log file
         df = pd.read_csv(fpath, sep=delimiter)
-        entries = df[labels[0]].values
+        entries = df[labels[0]].values.astype(float)
         is_entry = np.any(np.isclose(x, entries))
         if not is_entry:
 
@@ -192,6 +195,90 @@ def unmyelinatedFiberConvergence(pneuron, fiberD, rs, fiberL, maxNodeL_range, pp
 
     return df
 
+def unmyelinatedFiberConvergence_fiberL(pneuron, fiberD, rs, fiberL_range, pp, outdir='.'):
+
+    # Get filecode
+    fiber = unmyelinatedFiber(IintraFiber, pneuron, fiberD, rs, 5e-3)
+    filecodes = fiber.filecodes(IntracellularCurrent(fiber.nnodes // 2), 0.0, pp)
+    for k in ['nnodes', 'nodeL', 'interD', 'interL', 'psource', 'A', 'nature', 'toffset', 'PRF', 'DC']:
+        del filecodes[k]
+    filecodes['tstim'] = si_format(pp.tstim, 1, space='') + 's'
+    filecodes['fiberL_range'] = 'fiberL' + '-'.join(
+        [f'{si_format(x, 1, "")}m' for x in [min(fiberL_range), max(fiberL_range)]])
+    fcode = '_'.join(filecodes.values())
+    fiber.clear()
+
+    # Output file and column names
+    fname = f'{fcode}_convergence_results.csv'
+    fpath = os.path.join(outdir, fname)
+    delimiter = '\t'
+    labels = ['fiberL (m)', 'nodeL (m)', 'Ithr (A)', 'CV (m/s)', 'dV (mV)']
+
+    # Create log file if it does not exist
+    if not os.path.isfile(fpath):
+        logger.info(f'creating log file: "{fpath}"')
+        with open(fpath, 'w') as csvfile:
+            writer = csv.writer(csvfile, delimiter=delimiter)
+            writer.writerow(labels)
+
+    # Loop through parameter sweep
+    logger.info('running fiberL parameter sweep ({}m - {}m)'.format(
+        *si_format([fiberL_range.min(), fiberL_range.max()], 2)))
+    for x in fiberL_range[::-1]:
+
+        # If fiber length not already in the log file
+        df = pd.read_csv(fpath, sep=delimiter)
+        entries = df[labels[0]].values.astype(float)
+        is_entry = np.any(np.isclose(x, entries))
+        if not is_entry:
+
+            # Initialize fiber with specific fiber length
+            logger.info(f'creating model with fiberL = {x * 1e6:.2f} um ...')
+            fiber = unmyelinatedFiber(IintraFiber, pneuron, fiberD, rs, fiberL=x)
+            logger.info(f'resulting node length: {fiber.nodeL * 1e6:.2f} um')
+
+            # Perform titration to find threshold current
+            psource = IntracellularCurrent(fiber.nnodes // 2)
+            logger.info(f'running titration with intracellular current injected at node {psource.inode}')
+            Ithr = fiber.titrate(psource, pp)  # A
+
+            # If fiber is excited
+            if not np.isnan(Ithr):
+                logger.info(f'Ithr = {si_format(Ithr, 2)}A')
+
+                # Simulate fiber at 1.1 times threshold current
+                data, meta = fiber.simulate(psource, 1.1 * Ithr, pp)
+
+                # Filter out stimulation artefact from dataframe
+                data = {k: boundDataFrame(df, (pp.tstim, pp.tstim + pp.toffset)) for k, df in data.items()}
+
+                # Compute CV and spike amplitude
+                # ids = fiber.ids.copy()
+                # del ids[fiber.nnodes // 2]
+                cv = fiber.getConductionVelocity(data, out='median')  # m/s
+                dV = fiber.getSpikeAmp(data, out='median')            # mV
+                logger.info(f'CV = {cv:.2f} m/s')
+                logger.info(f'dV = {dV:.2f} mV')
+            else:
+                # Otherwise, assign NaN values to them
+                cv, dV = np.nan, np.nan
+
+            # Log input-output pair into file
+            logger.info('saving result to log file')
+            with open(fpath, 'a', newline='') as csvfile:
+                writer = csv.writer(csvfile, delimiter=delimiter)
+                writer.writerow([x, fiber.nodeL, Ithr, cv, dV])
+
+            # Clear fiber sections
+            fiber.clear()
+
+    logger.info('parameter sweep successfully completed')
+
+    # Load results
+    logger.info('loading results from log file')
+    df = pd.read_csv(fpath, sep=delimiter)
+
+    return df
 
 def myelinatedFiberReilly(fiber_class, fiberD=20e-6, **kwargs):
     '''  Create typical myelinated fiber model, using parameters from Reilly 1985.
@@ -245,5 +332,5 @@ def unmyelinatedFiberSundt(fiber_class, fiberD=0.8e-6, fiberL = 5e-3, **kwargs):
     '''
     pneuron = getPointNeuron('sundt')  # DRG peripheral axon membrane equations
     rs = 100.                          # axoplasm resistivity, from Sundt 2015 (Ohm.cm)
-    maxNodeL = 5e-6                   # maximum node length
+#    maxNodeL = 5e-6                   # maximum node length
     return unmyelinatedFiber(fiber_class, pneuron, fiberD, rs, fiberL, maxNodeL=maxNodeL, **kwargs)
